d75a62a37d6805fe9708f5216f370bf5f3f3bfcc
Maria Shodunke <mshodunke@ripple.com>
2025-05-23 14:05:07 +0100

    Test with lending env

--- docs/_code_samples/quickstart/py/single-asset-vault/create_vault.py
+++ docs/_code_samples/quickstart/py/single-asset-vault/create_vault.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
-from xrpl.utils import drops_to_xrp
 import json
 
 from xrpl_helpers import (
     get_account,
     get_account_info,
+    get_account_object,
     create_single_asset_vault,
+    update_single_asset_vault,
     set_ledger,
+    convert_drops_to_xrp
 )
 
 JSON_INDENT_VALUE = 2
@@ -34,12 +36,13 @@ class SAVTestHarness:
         ledger_frame = ttk.LabelFrame(master, text="Ledger", padding=10)
         ledger_frame.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
         ttk.Label(ledger_frame, text="Choose your ledger instance:", padding=5).grid(row=0, column=0, padx=5, pady=5, sticky="w")
-        self.ledger_instance = tk.StringVar(value="Testnet")
+        self.ledger_instance = tk.StringVar(value="Lending")
         ttk.Radiobutton(
             ledger_frame,
-            text="Testnet",
+            # TODO: Change to Testnet once testing complete
+            text="Lending",
             variable=self.ledger_instance,
-            value="Testnet",
+            value="Lending",
             bootstyle=PRIMARY,
             command=self.update_ledger
         ).grid(row=0, column=1, padx=5, pady=5, sticky="w")
@@ -47,8 +50,7 @@ class SAVTestHarness:
             ledger_frame,
             text="Devnet", 
             variable=self.ledger_instance,
-            # TODO: Change to Devnet once testing complete
-            value="Lending",
+            value="Devnet",
             bootstyle=PRIMARY,
             command=self.update_ledger
         ).grid(row=0, column=2, padx=5, pady=5, sticky="w")
@@ -56,9 +58,6 @@ class SAVTestHarness:
         ledger_frame.grid_columnconfigure(0, weight=1)
         ledger_frame.grid_columnconfigure(1, weight=0)
         ledger_frame.grid_columnconfigure(2, weight=0)
-
-         # Initialize the ledger network in xrpl_helpers
-        set_ledger(self.ledger_instance.get())
 
         # --- Vault Owner Section ---
         vault_owner_frame = ttk.LabelFrame(master, text="Vault Owner", padding=10)
@@ -69,7 +68,7 @@ class SAVTestHarness:
         self.vault_owner_seed_entry = ttk.Entry(vault_owner_frame, width=40) # Keep width for initial sizing
         self.vault_owner_seed_entry.grid(row=0, column=1, columnspan=2, padx=5, pady=5, sticky="ew")
 
-        ttk.Button(vault_owner_frame, text="Get VaultOwner Account", command=self.get_vault_owner_account, bootstyle=INFO + "-outline").grid(row=0, column=3, padx=5, pady=5, sticky="w")
+        ttk.Button(vault_owner_frame, text="Get Vault Owner Account", command=self.get_vault_owner_account, bootstyle=INFO + "-outline").grid(row=0, column=3, padx=5, pady=5, sticky="w")
 
         ttk.Label(vault_owner_frame, text="Vault Owner Account:", padding=5).grid(row=1, column=0, padx=5, pady=5, sticky="w")
         self.vault_owner_account_entry = ttk.Entry(vault_owner_frame, width=40)
@@ -99,16 +98,17 @@ class SAVTestHarness:
         self.depositor_xrp_balance_entry = ttk.Entry(depositor_frame, width=40, state="disabled")
         self.depositor_xrp_balance_entry.grid(row=2, column=1, columnspan=2, padx=5, pady=5, sticky="ew")
 
-
-
         # --- Vault Section ---
         vault_frame = ttk.LabelFrame(master, text="Vault", padding=10)
         vault_frame.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
         vault_frame.grid_columnconfigure(1, weight=1)
 
         ttk.Label(vault_frame, text="Currency (Asset):", padding=5).grid(row=0, column=0, padx=5, pady=5, sticky="w")
-        self.currency_entry = ttk.Entry(vault_frame, width=30)
+        self.currency_entry = ttk.Entry(vault_frame, width=30, state="disabled")
         self.currency_entry.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+        self.currency_entry.config(state="normal")
+        self.currency_entry.insert(tk.END, "XRP")
+        self.currency_entry.config(state="disabled")
 
         ttk.Label(vault_frame, text="Currency Maximum:", padding=5).grid(row=1, column=0, padx=5, pady=5, sticky="w")
         self.asset_maximum_entry = ttk.Entry(vault_frame, width=30)
@@ -131,14 +131,13 @@ class SAVTestHarness:
         self.shares_entry = ttk.Entry(vault_frame, width=30)
         self.shares_entry.grid(row=5, column=1, padx=5, pady=5, sticky="ew")
 
-        ttk.Button(vault_frame, text="Create Vault", command=self.create_vault, bootstyle=SUCCESS).grid(row=0, column=2, padx=5, pady=5, sticky="ew")
-        ttk.Button(vault_frame, text="Update Vault", command=self.update_vault, bootstyle=WARNING).grid(row=1, column=2, padx=5, pady=5, sticky="ew")
-        ttk.Button(vault_frame, text="Delete Vault", command=self.delete_vault, bootstyle=DANGER).grid(row=2, column=2, padx=5, pady=5, sticky="ew")
-        ttk.Button(vault_frame, text="Deposit", command=self.deposit, bootstyle=PRIMARY).grid(row=3, column=2, padx=5, pady=5, sticky="ew")
-        ttk.Button(vault_frame, text="Withdraw", command=self.withdraw, bootstyle=SECONDARY).grid(row=4, column=2, padx=5, pady=5, sticky="ew")
+        ttk.Button(vault_frame, text="Create Vault", command=self.create_vault, bootstyle=INFO).grid(row=0, column=2, padx=5, pady=5, sticky="ew")
+        ttk.Button(vault_frame, text="Update Vault", command=self.update_vault, bootstyle=INFO).grid(row=1, column=2, padx=5, pady=5, sticky="ew")
+        ttk.Button(vault_frame, text="Delete Vault", command=self.delete_vault, bootstyle=INFO).grid(row=2, column=2, padx=5, pady=5, sticky="ew")
+        ttk.Button(vault_frame, text="Deposit", command=self.deposit, bootstyle=INFO + "-outline").grid(row=3, column=2, padx=5, pady=5, sticky="ew")
+        ttk.Button(vault_frame, text="Withdraw", command=self.withdraw, bootstyle=INFO + "-outline").grid(row=4, column=2, padx=5, pady=5, sticky="ew")
         vault_frame.grid_columnconfigure(2, weight=1)
         vault_frame.grid_columnconfigure(3, weight=1)
-
 
         # --- Results Section ---
         results_frame = ttk.LabelFrame(master, text="Results", padding=5)
@@ -153,16 +152,16 @@ class SAVTestHarness:
         results_frame.grid_rowconfigure(0, weight=1)
         results_frame.grid_columnconfigure(0, weight=1)
 
-
     def update_ledger(self):
         """Updates the ledger instance."""
         selected_ledger = self.ledger_instance.get()
         ledger_network_url = set_ledger(selected_ledger)
-        self.update_results(f"Connecting to {selected_ledger}: {ledger_network_url}")
+        self.update_results(f"==== Connected to: {ledger_network_url} ====")
 
     def get_vault_owner_account(self):
         seed = self.vault_owner_seed_entry.get()
         vault_owner_account = get_account(seed)
+
         if not seed:
             self.vault_owner_seed_entry.delete(0, tk.END)
             self.vault_owner_seed_entry.insert(0, vault_owner_account.seed)
@@ -171,11 +170,11 @@ class SAVTestHarness:
         self.vault_owner_account_entry.delete(0, tk.END)
         self.vault_owner_account_entry.insert(0, vault_owner_account.classic_address)
 
+        self.update_results("\n===== Vault Owner Account Info ====")
         account_info = get_account_info(self.vault_owner_account_entry.get())
-        balance = float(drops_to_xrp(account_info['Balance']))
         self.vault_owner_xrp_balance_entry.config(state="normal")
         self.vault_owner_xrp_balance_entry.delete(0, tk.END)
-        self.vault_owner_xrp_balance_entry.insert(0, balance)
+        self.vault_owner_xrp_balance_entry.insert(0, convert_drops_to_xrp(account_info['Balance']))
         self.vault_owner_xrp_balance_entry.config(state="disabled")
 
         self.update_results(json.dumps(account_info, indent=JSON_INDENT_VALUE))
@@ -183,6 +182,7 @@ class SAVTestHarness:
     def get_depositor_account(self):
         seed = self.depositor_seed_entry.get()
         depositor_account = get_account(seed)
+
         if not seed:
             self.depositor_seed_entry.delete(0, tk.END)
             self.depositor_seed_entry.insert(0, depositor_account.seed)
@@ -191,26 +191,41 @@ class SAVTestHarness:
         self.depositor_account_entry.delete(0, tk.END)
         self.depositor_account_entry.insert(0, depositor_account.classic_address)
 
+        self.update_results("\n===== Depositor Account Info ====")
         account_info = get_account_info(self.depositor_account_entry.get())
-        balance = float(drops_to_xrp(account_info['Balance']))
         self.depositor_xrp_balance_entry.config(state="normal")
         self.depositor_xrp_balance_entry.delete(0, tk.END)
-        self.depositor_xrp_balance_entry.insert(0, balance)
+        self.depositor_xrp_balance_entry.insert(0, convert_drops_to_xrp(account_info['Balance']))
         self.depositor_xrp_balance_entry.config(state="disabled")
 
         self.update_results(json.dumps(account_info, indent=JSON_INDENT_VALUE))
 
     def create_vault(self):
-        result = create_single_asset_vault(
+        self.update_results(f"\n==== Preparing and Sending VaultCreate Transaction... ====")
+        tx, result  = create_single_asset_vault(
             self.vault_owner_seed_entry.get(),
-            self.currency_entry.get(),
             self.asset_maximum_entry.get(),
             self.enable_transferable_shares_var.get()
         )
+        self.update_results(json.dumps(tx, indent=JSON_INDENT_VALUE))
+        self.update_results(f"\n==== VaultCreate Result ====\n{json.dumps(result, indent=JSON_INDENT_VALUE)}")
 
-        self.update_results(json.dumps(result, indent=JSON_INDENT_VALUE))
+        vault_owner_account = self.vault_owner_account_entry.get()
+        vault = get_account_object(vault_owner_account, "VAULT")
+        if (vault["account_objects"]):
+            self.update_results(f"\n==== VaultCreate Result ====\n{json.dumps(vault, indent=JSON_INDENT_VALUE)}")
+            vault_id=vault["account_objects"][0]["index"]
+            self.vault_id_entry.insert(tk.END, vault_id + "\n")
+            self.vault_id_entry.see(tk.END)
+        
 
     def update_vault(self):
+        self.update_results(f"\n==== Preparing and Sending VaultSet Transaction... ====")
+        result = update_single_asset_vault(
+            self.vault_owner_seed_entry.get(),
+            self.asset_maximum_entry.get(),
+            self.enable_transferable_shares_var.get()
+        )
         vault_id = self.vault_id_entry.get()
         maximum = self.asset_maximum_entry.get()
         self.update_results(f"Attempting to update vault {vault_id} with new value for Currency Maximum: {maximum}")
@@ -237,7 +252,12 @@ class SAVTestHarness:
         self.results_text.insert(tk.END, text + "\n")
         self.results_text.see(tk.END)
         self.results_text.config(state="disabled")
-
+    
 root = tk.Tk()
 app = SAVTestHarness(root)
+
+# Display the default ledger network the app is connected to
+connected_network = set_ledger(app.ledger_instance.get())
+app.update_results(f"==== Connected to: {connected_network} ====")
+
 root.mainloop()
